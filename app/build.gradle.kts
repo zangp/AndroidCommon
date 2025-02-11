d2f53ffee77f07f8c612a55b09a573444cfc2d21
zangpeng <peng.zang@avatr.com>
2025-02-11 14:42:04 +0800

    init MainActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,20 @@
+import java.io.File
+import java.io.FileInputStream
+import java.util.*
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
 }
+
+val prop = Properties().apply {
+    load(FileInputStream(File(rootProject.rootDir, "local.properties")))
+}
+
+val sPassword = prop.getProperty("storePassword") ?: ""
+val sKeyAlias = prop.getProperty("keyAlias") ?: ""
+val sKeyPassword = prop.getProperty("keyPassword") ?: ""
+
 
 android {
     namespace = "com.perry.androidcommon"
@@ -17,9 +30,33 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        create("appDebug") {
+            storeFile = file("../avatrdebug.jks")
+            storePassword = sPassword
+            keyAlias = sKeyAlias
+            keyPassword = sKeyPassword
+        }
+        create("release") {
+            storeFile = file("../avatrdebug.jks")
+            storePassword = sPassword
+            keyAlias = sKeyAlias
+            keyPassword = sKeyPassword
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+
+        debug {
+            isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("appDebug")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -40,6 +77,8 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
