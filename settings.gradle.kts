5ada661abf6652d7d0dcbae5bef76b62375253f6
perry <perry@perrydeMacBook-Pro.local>
2025-03-22 23:19:13 +0800

    修改引用版本号

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,12 @@
 pluginManagement {
     repositories {
+        maven { url=uri ("https://www.jitpack.io")}
+        maven { url=uri ("https://maven.aliyun.com/repository/releases")}
+        maven { url=uri ("https://maven.aliyun.com/repository/google")}
+        maven { url=uri ("https://maven.aliyun.com/repository/central")}
+        maven { url=uri ("https://maven.aliyun.com/repository/gradle-plugin")}
+        maven { url=uri ("https://maven.aliyun.com/repository/public")}
+
         google {
             content {
                 includeGroupByRegex("com\\.android.*")
@@ -14,6 +21,12 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        maven { url=uri ("https://www.jitpack.io")}
+        maven { url=uri ("https://maven.aliyun.com/repository/releases")}
+        maven { url=uri ("https://maven.aliyun.com/repository/google")}
+        maven { url=uri ("https://maven.aliyun.com/repository/central")}
+        maven { url=uri ("https://maven.aliyun.com/repository/gradle-plugin")}
+        maven { url=uri ("https://maven.aliyun.com/repository/public")}
         google()
         mavenCentral()
     }
